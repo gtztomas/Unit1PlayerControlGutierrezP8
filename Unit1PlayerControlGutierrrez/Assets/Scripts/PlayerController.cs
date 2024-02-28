15d6649454266fe871bf698cde3404d77f75c09c
Tomas Gutierrez <723369@student.dusd.net>
2024-02-28 15:29:04 -0800

    fINISH project

--- Unit1PlayerControlGutierrrez/Assets/Scripts/PlayerController.cs
+++ Unit1PlayerControlGutierrrez/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
+    //Private Variables
+    private float speed = 15.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -11,10 +21,24 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float speed = 20;
+
     void Update()
     {
-        //Move the veichle forward 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Axis setup
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+
     }
 }
